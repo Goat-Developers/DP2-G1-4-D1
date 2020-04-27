87470636aa65a1374a6beb0a5eca8e064f24d659
chaoh <chaoh@Ideapad520.home>
2020-04-27 18:07:11 +0200

    UI Test de añadir y eliminar vacuna

---
+++ src/test/java/org/springframework/samples/petclinic/ui/DeleteVaccineUITest.java
@@ -0,0 +1,109 @@
+package org.springframework.samples.petclinic.ui;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class DeleteVaccineUITest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeEach
+  public void setUp() throws Exception {
+
+	String pathToGeckoDriver="./target/classes/static/resources/";
+	System.setProperty("webdriver.gecko.driver", pathToGeckoDriver + "geckodriver.exe");
+    driver = new FirefoxDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+  
+  @Test
+	public void testDeleteVaccine() throws Exception {
+		as("vet1")
+		.whenIamLoggedIntheSystem()
+		.thenICanDeleteAVaccine();
+	}
+	
+	private void thenICanDeleteAVaccine() {
+	    driver.findElement(By.linkText("Eliminar vacuna")).click();
+	}
+	
+	private DeleteVaccineUITest whenIamLoggedIntheSystem() {	
+		return this;
+	}
+
+	private DeleteVaccineUITest as(String username) {
+    driver.get("http://localhost:8080/");
+    driver.findElement(By.xpath("//div[@id='main-navbar']/ul[2]/li/a")).click();
+    driver.findElement(By.id("username")).clear();
+    driver.findElement(By.id("username")).sendKeys("vet1");
+    driver.findElement(By.id("password")).click();
+    driver.findElement(By.id("password")).clear();
+    driver.findElement(By.id("password")).sendKeys(passwordOfVet());
+    driver.findElement(By.id("password")).sendKeys(Keys.ENTER);
+    driver.findElement(By.linkText("Vacunas")).click();
+    driver.findElement(By.linkText("Vacunas caducadas")).click();
+    return this;
+  }
+	
+	private CharSequence passwordOfVet() {
+		return "v3t";
+	}
+
+
+  @AfterEach
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
